e57c22dbac470683529163beff4308f2a484a912
tahsinsakib5 <138654404+tahsinsakib5@users.noreply.github.com>
2023-09-21 11:30:12 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+   WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(const MyApp());
 }
 
